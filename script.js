310424cf04d7731b4f8f7851ae5d6811b87f4d50
MANTHANGUPTA8783 <124601427+MANTHANGUPTA8783@users.noreply.github.com>
2023-05-23 01:42:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let yes=document.getElementById("yes");
+console.log(yes);
+let wind=window;
+console.log(wind)
+yes.addEventListener('click',function(){
+    alert("You Will be redirected to main Page")
+})
